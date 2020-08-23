653b8101dc4fe468524d82c090bb8a3546331f23
DanyMS99 <54692475+DanyMS99@users.noreply.github.com>
2020-08-23 11:07:34 -0600

    corregido

--- Tic-Tac-Toe/Tic-Tac-Toe/Form1.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Form1.cs
@@ -94,65 +94,32 @@ namespace Tic_Tac_Toe
                     else if (!win)
                     {
                         //Turno Inteligencia
-                        int pos;
-                        bool IA = false;
-                        do
+                        //PRIORIDADES
+                        //1.- VERIFICAR SI PUEDE GANAR IA
+                        string IAKanojo = BuscarGanarOBloquear("O");
+                        if (IAKanojo == "")
                         {
-                            pos = r.Next(1, 10);
-                            if (!comprobar(TicTac, pos.ToString()))
+                            //2.- BLOQUEAR AL JUGADOR1 DE QUE GANE
+                            IAKanojo = BuscarGanarOBloquear("X");
+                            if (IAKanojo == "")
                             {
-                                IA = true;
-                                switch (pos.ToString())
+                                //3.- BUSCAR ESQUINAS
+                                IAKanojo = BuscarEsquina();
+                                if (IAKanojo == "")
                                 {
-                                    case "1":
-                                        pictureBox7.BackgroundImage = Properties.Resources.punto;
-                                        TicTac[2, 0] = "O";
-                                        count++;
-                                        break;
-                                    case "2":
-                                        pictureBox8.BackgroundImage = Properties.Resources.punto;
-                                        TicTac[2, 1] = "O";
-                                        count++;
-                                        break;
-                                    case "3":
-                                        pictureBox9.BackgroundImage = Properties.Resources.punto;
-                                        TicTac[2, 2] = "O";
-                                        count++;
-                                        break;
-                                    case "4":
-                                        pictureBox4.BackgroundImage = Properties.Resources.punto;
-                                        TicTac[1, 0] = "O";
-                                        count++;
-                                        break;
-                                    case "5":
-                                        pictureBox5.BackgroundImage = Properties.Resources.punto;
-                                        TicTac[1, 1] = "O";
-                                        count++;
-                                        break;
-                                    case "6":
-                                        pictureBox6.BackgroundImage = Properties.Resources.punto;
-                                        TicTac[1, 2] = "O";
-                                        count++;
-                                        break;
-                                    case "7":
-                                        pictureBox1.BackgroundImage = Properties.Resources.punto;
-                                        TicTac[0, 0] = "O";
-                                        count++;
-                                        break;
-                                    case "8":
-                                        pictureBox2.BackgroundImage = Properties.Resources.punto;
-                                        TicTac[0, 1] = "O";
-                                        count++;
-                                        break;
-                                    case "9":
-                                        pictureBox3.BackgroundImage = Properties.Resources.punto;
-                                        TicTac[0, 2] = "O";
-                                        count++;
-                                        break;
+                                    //4.- BUSCAR ESPACIO VACIO
+                                    IAKanojo = BuscarEspacioVacio();
+                                    if (IAKanojo != "")
+                                        IA(IAKanojo);
                                 }
+                                else
+                                    IA(IAKanojo);
                             }
-                        } while (!IA);
-                        //CheckWinnerO
+                            else
+                                IA(IAKanojo);
+                        }
+                        else
+                            IA(IAKanojo);                     
                         if (checkWin("O"))
                         {
                             win = true;
@@ -171,6 +138,177 @@ namespace Tic_Tac_Toe
                     m.ShowDialog();
                 }
             }
+        }
+        public void IA(string pos)
+        {
+            switch (pos)
+            {
+                case "1":
+                    pictureBox7.BackgroundImage = Properties.Resources.punto;
+                    TicTac[2, 0] = "O";
+                    count++;
+                    break;
+                case "2":
+                    pictureBox8.BackgroundImage = Properties.Resources.punto;
+                    TicTac[2, 1] = "O";
+                    count++;
+                    break;
+                case "3":
+                    pictureBox9.BackgroundImage = Properties.Resources.punto;
+                    TicTac[2, 2] = "O";
+                    count++;
+                    break;
+                case "4":
+                    pictureBox4.BackgroundImage = Properties.Resources.punto;
+                    TicTac[1, 0] = "O";
+                    count++;
+                    break;
+                case "5":
+                    pictureBox5.BackgroundImage = Properties.Resources.punto;
+                    TicTac[1, 1] = "O";
+                    count++;
+                    break;
+                case "6":
+                    pictureBox6.BackgroundImage = Properties.Resources.punto;
+                    TicTac[1, 2] = "O";
+                    count++;
+                    break;
+                case "7":
+                    pictureBox1.BackgroundImage = Properties.Resources.punto;
+                    TicTac[0, 0] = "O";
+                    count++;
+                    break;
+                case "8":
+                    pictureBox2.BackgroundImage = Properties.Resources.punto;
+                    TicTac[0, 1] = "O";
+                    count++;
+                    break;
+                case "9":
+                    pictureBox3.BackgroundImage = Properties.Resources.punto;
+                    TicTac[0, 2] = "O";
+                    count++;
+                    break;
+            }
+        }
+        string BuscarEspacioVacio()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is PictureBox)
+                    if (c.BackgroundImage == null)
+                        return c.Tag.ToString();
+            }
+            return "";
+        }
+        string BuscarEsquina()
+        {
+            if(TicTac[0, 0] == "O")
+            {
+                if (TicTac[0, 2] == "")
+                    return "9";
+                if (TicTac[2, 2] == "")
+                    return "3";
+                if (TicTac[2, 0] == "")
+                    return "1";
+            }
+            if (TicTac[0, 2] == "O")
+            {
+                if (TicTac[0, 0] == "")
+                    return "7";
+                if (TicTac[2, 0] == "")
+                    return "1";
+                if (TicTac[2, 2] == "")
+                    return "3";                
+            }
+            if (TicTac[2, 0] == "O")
+            {
+                if (TicTac[2, 2] == "")
+                    return "3";
+                if (TicTac[0, 2] == "")
+                    return "9";
+                if (TicTac[0, 0] == "")
+                    return "7";
+            }
+            if (TicTac[2, 2] == "O")
+            {
+                if (TicTac[2, 0] == "")
+                    return "1";
+                if (TicTac[0, 2] == "")
+                    return "9";
+                if (TicTac[0, 0] == "")
+                    return "7";
+            }
+            if (TicTac[2, 0] == "")
+                return "1";
+            if (TicTac[0, 2] == "")
+                return "9";
+            if (TicTac[0, 0] == "")
+                return "7";
+            if (TicTac[2, 2] == "")
+                return "3";
+            return "";
+        }
+        string BuscarGanarOBloquear(string symbol)
+        {
+            //Horizontalmente
+            //primera linea h
+            if (TicTac[0, 0] == symbol && TicTac[0, 1] == symbol && TicTac[0, 2] == "")
+                return "9";
+            if (TicTac[0, 0] == symbol && TicTac[0, 1] == "" && TicTac[0, 2] == symbol)
+                return "8";
+            if (TicTac[0, 0] == "" && TicTac[0, 1] == symbol && TicTac[0, 2] == symbol)
+                return "7";
+            //segunda linea h
+            if (TicTac[1, 0] == symbol && TicTac[1, 1] == symbol && TicTac[1, 2] == "")
+                return "6";
+            if (TicTac[1, 0] == symbol && TicTac[1, 1] == "" && TicTac[1, 2] == symbol)
+                return "5";
+            if (TicTac[1, 0] == "" && TicTac[1, 1] == symbol && TicTac[1, 2] == symbol)
+                return "4";
+            //tercer linea h
+            if (TicTac[2, 0] == symbol && TicTac[2, 1] == symbol && TicTac[2, 2] == "")
+                return "3";
+            if (TicTac[2, 0] == symbol && TicTac[2, 1] == "" && TicTac[2, 2] == symbol)
+                return "2";
+            if (TicTac[2, 0] == "" && TicTac[2, 1] == symbol && TicTac[2, 2] == symbol)
+                return "1";
+            //Verticalmente
+            //primer linea v
+            if (TicTac[0, 0] == symbol && TicTac[1, 0] == symbol && TicTac[2, 0] == "")
+                return "1";
+            if (TicTac[0, 0] == symbol && TicTac[1, 0] == "" && TicTac[2, 0] == symbol)
+                return "4";
+            if (TicTac[0, 0] == "" && TicTac[1, 0] == symbol && TicTac[2, 0] == symbol)
+                return "7";
+            //segunda linea v
+            if (TicTac[0, 1] == symbol && TicTac[1, 1] == symbol && TicTac[2, 1] == "")
+                return "2";
+            if (TicTac[0, 1] == symbol && TicTac[1, 1] == "" && TicTac[2, 1] == symbol)
+                return "5";
+            if (TicTac[0, 1] == "" && TicTac[1, 1] == symbol && TicTac[2, 1] == symbol)
+                return "8";
+            //tercer linea v
+            if (TicTac[0, 2] == symbol && TicTac[1, 2] == symbol && TicTac[2, 2] == "")
+                return "3";
+            if (TicTac[0, 2] == symbol && TicTac[1, 2] == "" && TicTac[2, 2] == symbol)
+                return "6";
+            if (TicTac[0, 2] == "" && TicTac[1, 2] == symbol && TicTac[2, 2] == symbol)
+                return "9";
+            //Diagonal 7 - 3
+            if (TicTac[0, 0] == "" && TicTac[1, 1] == symbol && TicTac[2, 2] == symbol)
+                return "7";
+            if (TicTac[0, 0] == symbol && TicTac[1, 1] == "" && TicTac[2, 2] == symbol)
+                return "5";
+            if (TicTac[0, 0] == symbol && TicTac[1, 1] == symbol && TicTac[2, 2] == "")
+                return "3";
+            //Diagonal 1 - 9
+            if (TicTac[2, 0] == "" && TicTac[1, 1] == symbol && TicTac[0, 2] == symbol)
+                return "1";
+            if (TicTac[2, 0] == symbol && TicTac[1, 1] == "" && TicTac[0, 2] == symbol)
+                return "5";
+            if (TicTac[2, 0] == symbol && TicTac[1, 1] == symbol && TicTac[0, 2] == "")
+                return "9";
+            return "";
         }
         bool checkWin(string symbol)
         {
